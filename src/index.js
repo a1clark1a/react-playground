3bab80a04ff24f58104a005bc8ebd00b9ab1d5a8
Anthony Clark Perfecto <a1clark1a@gmail.com>
2019-12-10 22:15:07 -0800

    Completed Paltrows-power-toes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import AppPal from "./paltrows-power-toes/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <AppPal />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
