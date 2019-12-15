0c622976702109427bfa708d5ccefd0426220756
Anthony Clark Perfecto <a1clark1a@gmail.com>
2019-12-14 21:53:51 -0800

    Added Lang-Control Context project

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import AppPal from "./paltrows-power-toes/App";
+import AppLang from "./lang-context/AppLang";
+
+//Use App for project: composition, demonymApp, leapyearprogram, state_drills and state
+//Use AppPal for project: paltrows-power-toes
 
 ReactDOM.render(
   <BrowserRouter>
-    <AppPal />
+    <AppLang />
   </BrowserRouter>,
   document.getElementById("root")
 );
---
+++ src/lang-context/LangControls.js
@@ -0,0 +1,40 @@
+import React from "react";
+import LanguageContext from "./LanguageContext";
+
+export default function LangControls(props) {
+  return (
+    <LanguageContext.Consumer>
+      {value => {
+        console.log(value);
+        return (
+          <>
+            <button disabled={value.lang === "en-GB"}>
+              British{" "}
+              <span role="img" aria-label="en-GB">
+                🇬🇧
+              </span>
+            </button>{" "}
+            <button disabled={value.lang === "en-US"}>
+              American{" "}
+              <span role="img" aria-label="en-US">
+                🇺🇸
+              </span>
+            </button>{" "}
+            <button disabled={value.lang === "ko"}>
+              Korean{" "}
+              <span role="img" aria-label="ko">
+                🇰🇷
+              </span>
+            </button>{" "}
+            <button disabled={value.lang === "tag"}>
+              Tagalog{" "}
+              <span role="img" aria-label="tag">
+                tag
+              </span>
+            </button>
+          </>
+        );
+      }}
+    </LanguageContext.Consumer>
+  );
+}
